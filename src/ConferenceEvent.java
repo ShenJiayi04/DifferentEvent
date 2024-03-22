1ea0cf2e600e9232db5788fe106e6789a07e1096
ShenJiayi <x00217230204@njit.edu.cn>
2024-03-22 19:18:08 +0800

    first commit

---
+++ src/ConferenceEvent.java
@@ -0,0 +1,63 @@
+public class  ConferenceEvent extends Event {
+    //private boolean breakfastRequired;
+    private double breakfastCost;
+    //private boolean lunchRequired;
+    private double lunchCost;
+    //private boolean dinnerRequired;
+    private double dinnerCost;
+    private double conferenceEventCost;
+
+    
+
+   public double getBreakfastCost() {
+       return breakfastCost;
+   }
+
+   public void setBreakfastCost(double breakfastCost) {
+       this.breakfastCost = breakfastCost;
+   }
+
+   
+
+   public double getLunchCost() {
+       return lunchCost;
+   }
+
+   public void setLunchCost(double lunchCost) {
+       this.lunchCost = lunchCost;
+   }
+
+   
+
+   public double getDinnerCost() {
+       return dinnerCost;
+   }
+
+   public void setDinnerCost(double dinnerCost) {
+       this.dinnerCost = dinnerCost;
+   }
+
+    public ConferenceEvent(String eventID,String eventName,String eventLocation,String pointOfContact,int totalParticipants,int totalEventDays,double eventCost,double breakfastCost,double lunchCost,double dinnerCost){
+        super(eventID,eventName,eventLocation,pointOfContact,eventCost,totalParticipants,totalEventDays);
+        this.breakfastCost=breakfastCost;
+        this.lunchCost=lunchCost;
+        this.dinnerCost=dinnerCost;
+    }
+
+    @Override
+    public  void calculateEventCost(){
+        conferenceEventCost = getEventCost()+((getBreakfastCost()+getLunchCost()+getDinnerCost())*getTotalParticipants()*getTotalEventDays());
+    }
+    
+
+    @Override
+    public String toString(){
+        //String conferenceEventCost;
+        return "Conference Event details: "+"\n"+
+        "Event ID: "+getEventID()+"\n"+
+        "Event Name: "+getEventName()+"\n"+
+        "Event Location: "+getEventLocation()+"\n"+
+        "Total participants: "+getTotalParticipants()+"\n"+
+        "Total Conference Cost: "+ conferenceEventCost;
+    }
+}
